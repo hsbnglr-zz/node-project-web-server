208d9b5285614e05ac3444232f328802e6701c8e
Sharma <hemant.sharma@bt.com>
2017-09-10 21:39:43 +0530

    setup start script and heroku port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 let app = express();
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
@@ -37,4 +38,4 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(port);
